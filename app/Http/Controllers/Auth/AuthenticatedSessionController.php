ced61af5f327ff82b732edb1e798ffe5b0f4985e
David Martin <home@sirrio.de>
2025-06-04 23:12:12 +0200

    Fix registration controller file

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class AuthenticatedSessionController extends Controller
 {
+  /**
+   * Display the login page.
+   */
+  public function create()
+  {
+    return Inertia::render('login');
+  }
+
+  /**
+   * Handle an authentication request.
+   */
+  public function store(LoginRequest $request): RedirectResponse
+  {
+    $request->authenticate();
+    $request->session()->regenerate();
+
+    return redirect()->route('characters.index');
+  }
+
   /**
    * Destroy an authenticated session.
    */
--- routes/auth.php
+++ routes/auth.php
@@ -1,11 +1,18 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\SocialAuthController;
 use App\Http\Controllers\Character\SyncAccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
+  Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+  Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+  Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+  Route::post('/register', [RegisteredUserController::class, 'store']);
+
   Route::get('/auth/redirect', [SocialAuthController::class, 'redirectToProvider'])
     ->name('discord.login');
 
