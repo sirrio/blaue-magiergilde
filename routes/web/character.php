dee3c9978d2897605da79d93060f6c4450b1056f
David Martin <home@sirrio.de>
2025-07-28 00:22:51 +0200

    Fix deleted characters route conflict

--- routes/web/character.php
+++ routes/web/character.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\Character\DeletedCharacterController;
 use App\Http\Controllers\Character\SortCharacterController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('characters/deleted', DeletedCharacterController::class)
+    ->middleware(['auth'])
+    ->name('characters.deleted');
+
 Route::resource('characters', CharacterController::class)->only([
     'index',
     'show',
@@ -15,7 +19,8 @@ Route::resource('characters', CharacterController::class)->only([
     'destroy',
 ])->middleware(['auth']);
 
-Route::post('characters/sort', SortCharacterController::class)->middleware(['auth'])->name('characters.sort');
-Route::get('characters/deleted', DeletedCharacterController::class)->middleware(['auth'])->name('characters.deleted');
+Route::post('characters/sort', SortCharacterController::class)
+    ->middleware(['auth'])
+    ->name('characters.sort');
 Route::post('characters/{character}/permanent-delete', PermanentDeleteCharacterController::class)->middleware(['auth'])->name('characters.permanent-delete');
 Route::post('characters/{character}/restore-deleted', RestoreDeletedCharacterController::class)->middleware(['auth'])->name('characters.restore-deleted');
