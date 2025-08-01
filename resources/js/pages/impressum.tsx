f334b1b088b9566be54861799469254b46b50343
sirrio <home@sirrio.de>
2025-08-01 15:40:12 +0200

    Some formating

--- resources/js/pages/impressum.tsx
+++ resources/js/pages/impressum.tsx
@@ -29,22 +29,13 @@ export default function Impressum() {
               <CardTitle className="justify-center">
                 <h1>Impressum</h1>
               </CardTitle>
-              <h2>Angaben gemäß § 5 DDG</h2>
+              <h2 className="font-bold">Angaben gemäß § 5 TMG (Telemediengesetz):</h2>
               <p>{VITE_IMPRESSUM_NAME}</p>
               <p>{VITE_IMPRESSUM_STREET}</p>
               <p>{VITE_IMPRESSUM_CITY}</p>
-              <h2>Vertreten durch:</h2>
-              <p>{VITE_IMPRESSUM_NAME}</p>
-              <h2>Kontakt:</h2>
-              <p>Telefon: {VITE_IMPRESSUM_PHONE}</p>
-              <p>Fax: {VITE_IMPRESSUM_FAX}</p>
+              <p>Website-Betreiber: Diese Webseite wird von {VITE_IMPRESSUM_NAME} als Privatperson betrieben (kein kommerzielles Angebot).</p>
+              <h2 className="font-bold">Kontakt:</h2>
               <p>E-Mail: {VITE_IMPRESSUM_EMAIL}</p>
-              <h2>Umsatzsteuer-ID:</h2>
-              <p>Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz: Musterustid.</p>
-              <h2>Wirtschafts-ID:</h2>
-              <p>Musterwirtschaftsid</p>
-              <h2>Aufsichtsbehörde:</h2>
-              <p>Musteraufsicht Musterstadt</p>
               <h2>Haftungsausschluss:</h2>
               <h3>Haftung für Inhalte</h3>
               <p>
@@ -53,29 +44,29 @@ export default function Impressum() {
                 übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
                 Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
               </p>
-              <h3>Haftung für Links</h3>
+              <h3 className="font-bold">Haftung für Links</h3>
               <p>
                 Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen.
                 Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
                 Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
               </p>
-              <h3>Urheberrecht</h3>
+              <h3 className="font-bold">Urheberrecht</h3>
               <p>
                 Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
                 Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet.
                 Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
               </p>
-              <h3>Datenschutz</h3>
+              <h3 className="font-bold">Datenschutz</h3>
               <p>
                 Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis.
                 Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann.
                 Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen.
                 Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
               </p>
-              <h2>Attribution</h2>
+              <h2 className="font-bold">Attribution</h2>
               <p>
                 Logo-Design von{' '}
-                <a href="https://linktr.ee/lizzylizarts" target="_blank" rel="noopener noreferrer">
+                <a className="link" href="https://linktr.ee/lizzylizarts" target="_blank" rel="noopener noreferrer">
                   lizzylizarts
                 </a>
                 .
