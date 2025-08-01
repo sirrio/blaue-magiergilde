3837104be12022bf18228d5160c836737a17b020
sirrio <home@sirrio.de>
2025-08-01 16:19:53 +0200

    Also updated favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,11 +42,11 @@
   <link rel="preconnect" href="https://fonts.bunny.net">
   <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-  <!-- Favicons -->
   <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
   <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
   <link rel="shortcut icon" href="/favicons/favicon.ico" />
   <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="Blaue Magiergilde" />
   <link rel="manifest" href="/favicons/site.webmanifest" />
 
   @routes
